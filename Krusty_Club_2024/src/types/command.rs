85f9626ba9199b11009645aaafeb6f0345a5f368
Oussema Kasraoui <oussema.kasraoui@studenti.unitn.it>
2024-11-24 16:46:38 +0100

    Delete Krusty_Club_2024 directory(outdated)

--- Krusty_Club_2024/src/types/command.rs
+++
@@ -1,9 +0,0 @@
-use crate::types::packet::Packet;
-use crate::types::source_routing_header::NodeId;
-use crossbeam_channel::Sender;
-
-pub enum Command {
-    AddChannel(NodeId, Sender<Packet>),
-    RemoveChannel(NodeId),
-    Crash,
-}
