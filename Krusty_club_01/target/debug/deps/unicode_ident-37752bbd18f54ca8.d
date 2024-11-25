4891e0ac14daf3882f24ead6f58375d0b835bd7d
Oussema Kasraoui <oussema.kasraoui@studenti.unitn.it>
2024-11-25 21:29:07 +0100

    protocol updated

---
+++ Krusty_club_01/target/debug/deps/unicode_ident-37752bbd18f54ca8.d
@@ -0,0 +1,8 @@
+C:\Users\oussk\RustroverProjects\Krusty_club_01\target\debug\deps\libunicode_ident-37752bbd18f54ca8.rmeta: C:\Users\oussk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs C:\Users\oussk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs
+
+C:\Users\oussk\RustroverProjects\Krusty_club_01\target\debug\deps\libunicode_ident-37752bbd18f54ca8.rlib: C:\Users\oussk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs C:\Users\oussk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs
+
+C:\Users\oussk\RustroverProjects\Krusty_club_01\target\debug\deps\unicode_ident-37752bbd18f54ca8.d: C:\Users\oussk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs C:\Users\oussk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs
+
+C:\Users\oussk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs:
+C:\Users\oussk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs:
