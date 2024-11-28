993459245154fe2fb1cce2ac7027ae32209898fc
Oussema Kasraoui <oussema.kasraoui@studenti.unitn.it>
2024-11-28 16:13:03 +0100

    adding the changes we made in drone.rs, command.rs

---
+++ crates/wg_packet/src/flood.rs
@@ -0,0 +1,21 @@
+use wg_network::NodeId;
+
+#[derive(Debug, Clone)]
+pub enum NodeType {
+    Client,
+    Drone,
+    Server,
+}
+
+#[derive(Debug, Clone)]
+pub struct FloodRequest {
+    pub flood_id: u64,
+    pub initiator_id: NodeId,
+    pub path_trace: Vec<(NodeId, NodeType)>,
+}
+
+#[derive(Debug, Clone)]
+pub struct FloodResponse {
+    pub flood_id: u64,
+    pub path_trace: Vec<(NodeId, NodeType)>,
+}
