553db51789e991a8eb2ad318cac52006c0ca8897
Oussema Kasraoui <oussema.kasraoui@studenti.unitn.it>
2024-12-02 20:10:06 +0100

    Update drone.rs

--- Krusty_club_01/crates/wg_drone/src/drone.rs
+++ Krusty_club_01/crates/wg_drone/src/drone.rs
@@ -1,19 +1,9 @@
 use std::collections::HashMap;
 
 use crossbeam_channel::{Receiver, Sender};
-use wg_controller::Command;
+use wg_controller::{DroneCommand, NodeEvent};
 use wg_network::NodeId;
 use wg_packet::Packet;
-
-#[derive(Debug, Clone)]
-pub struct DroneOptions {
-    pub id: NodeId,
-    pub sim_contr_send: Sender<Command>,
-    pub sim_contr_recv: Receiver<Command>,
-    pub packet_recv: Receiver<Packet>,
-    pub packet_send: HashMap<NodeId, Sender<Packet>>,
-    pub pdr: f32,
-}
 
 /// This is the drone interface.
 /// Each drone's group must implement it
@@ -21,7 +11,14 @@ pub trait Drone {
     /// The list packet_send would be crated empty inside new.
     /// Other nodes are added by sending command
     /// using the simulation control channel to send 'Command(AddChannel(...))'.
-    fn new(options: DroneOptions) -> Self;
+    fn new(
+        id: NodeId,
+        controller_send: Sender<NodeEvent>,
+        controller_recv: Receiver<DroneCommand>,
+        packet_recv: Receiver<Packet>,
+        packet_send: HashMap<NodeId, Sender<Packet>>,
+        pdr: f32,
+    ) -> Self;
 
     fn run(&mut self);
 }
