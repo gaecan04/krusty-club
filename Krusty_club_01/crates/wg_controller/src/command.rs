9d62cc07029f185c941e27177c14d335dbc50d94
Oussema Kasraoui <oussema.kasraoui@studenti.unitn.it>
2024-12-02 20:08:26 +0100

    Update command.rs

--- Krusty_club_01/crates/wg_controller/src/command.rs
+++ Krusty_club_01/crates/wg_controller/src/command.rs
@@ -2,9 +2,34 @@ use crossbeam_channel::Sender;
 use wg_network::NodeId;
 use wg_packet::Packet;
 
+/// From controller to drone
 #[derive(Debug, Clone)]
-pub enum Command {
-    AddChannel(NodeId, Sender<Packet>),
-    RemoveChannel(NodeId),
+pub enum DroneCommand {
+    AddSender(NodeId, Sender<Packet>),
+    SetPacketDropRate(f32),
     Crash,
+}
+
+#[cfg(feature = "partial_eq")]
+impl PartialEq for DroneCommand {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (DroneCommand::AddSender(node1, sender1), DroneCommand::AddSender(node2, sender2)) => {
+                node1 == node2 && sender1.same_channel(sender2)
+            }
+            (DroneCommand::SetPacketDropRate(rate1), DroneCommand::SetPacketDropRate(rate2)) => {
+                rate1 == rate2
+            }
+            (DroneCommand::Crash, DroneCommand::Crash) => true,
+            _ => false,
+        }
+    }
+}
+
+/// From drone to controller
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "partial_eq", derive(PartialEq))]
+pub enum NodeEvent {
+    PacketSent(Packet),
+    PacketDropped(Packet),
 }
